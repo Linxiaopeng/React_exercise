b34f633bfe8dc8e21f075c1c0c8df805ab6f895c
Linxiaopeng <linxiaopeng2013@gmail.com>
2018-08-14 01:05:12 +0800

    using JSX describe UI information

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,15 +1,23 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <h1>React 小书</h1>
-      </div>
-    );
-  }
+// class Header extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>React 小书</h1>
+//         <h2> render title </h2>
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<Header />, document.getElementById("root"));
+
+function renderContent(content) {
+  ReactDOM.render(<h1>{content}</h1>, document.getElementById("root"));
 }
 
-ReactDOM.render(<Header />, document.getElementById("root"));
+renderContent("Hello World");
