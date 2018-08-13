85a9be81c19a80e5db02c1d3d8eab36999c771dd
Linxiaopeng <linxiaopeng2013@gmail.com>
2018-08-14 01:32:00 +0800

    lesson7

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,23 +1,29 @@
-// import React, { Component } from "react";
-import React from "react";
+import React, { Component } from "react";
+// import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// class Header extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>React 小书</h1>
-//         <h2> render title </h2>
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<Header />, document.getElementById("root"));
-
-function renderContent(content) {
-  ReactDOM.render(<h1>{content}</h1>, document.getElementById("root"));
+class Header extends Component {
+  render() {
+    const isGoodWord = false;
+    const goodWord = <strong> is good </strong>;
+    const badWord = <strong> is not good </strong>;
+    return (
+      <div>
+        <h1>
+          React Book
+          {isGoodWord ? goodWord : badWord}
+        </h1>
+        <h2> render title </h2>
+      </div>
+    );
+  }
 }
 
-renderContent("Hello World");
+ReactDOM.render(<Header />, document.getElementById("root"));
+
+// function renderContent(content) {
+//   ReactDOM.render(<h1>{content}</h1>, document.getElementById("root"));
+// }
+
+// renderContent("Hello World");
