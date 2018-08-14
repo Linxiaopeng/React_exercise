55f3826ba3d92a574a28facd809d504bcf9b856e
linxiaopeng <linxiaopeng2013@gail.com>
2018-08-14 14:45:50 +0800

    lesson 8

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,21 +2,53 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+class Title extends Component {
+  render() {
+    return <h1>React</h1>;
+  }
+}
+
 class Header extends Component {
   render() {
-    const isGoodWord = false;
-    const goodWord = <strong> is good </strong>;
-    const badWord = <strong> is not good </strong>;
     return (
       <div>
-        <h1>
-          React Book
-          {isGoodWord ? goodWord : badWord}
-        </h1>
-        <h2> render title </h2>
+        <Title />
+        <h2>This is Header</h2>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Header />, document.getElementById("root"));
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is main content</h2>
+      </div>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is footer</h2>
+      </div>
+    );
+  }
+}
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
