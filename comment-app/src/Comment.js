f8d9067339030eda8952f46fe2490964452d048e
linxiaopeng <linxiaopeng2013@gmail.com>
2018-08-29 19:30:54 +0800

    lesson 26

--- comment-app/src/Comment.js
+++ comment-app/src/Comment.js
@@ -3,8 +3,29 @@ import PropTypes from "prop-types";
 
 class Comment extends Component {
   static propTypes = {
-    comment: PropTypes.object
+    comment: PropTypes.object.isRequired
   };
+
+  constructor() {
+    super();
+    this.state = { timeString: "" };
+  }
+
+  componentWillMount() {
+    this._updateTimeString();
+    this._timer = setInterval(this._updateTimeString.bind(this), 5000);
+  }
+
+  _updateTimeString() {
+    const comment = this.props.comment;
+    const duration = (+Date.now() - comment.createdTime) / 1000;
+    this.setState({
+      timeString:
+        duration > 60
+          ? `${Math.round(duration / 60)} 分钟前`
+          : `${Math.round(Math.max(duration, 1))} 秒前`
+    });
+  }
 
   render() {
     return (
@@ -13,6 +34,7 @@ class Comment extends Component {
           <span>{this.props.comment.username} </span>：
         </div>
         <p>{this.props.comment.content}</p>
+        <span className="comment-createdtime">{this.state.timeString}</span>
       </div>
     );
   }
